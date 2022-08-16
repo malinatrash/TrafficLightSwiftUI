5c208c01bf045eb65268f8b339196f9aac5089e9
malinatrash <malinatrashh@gmail.com>
2022-08-16 18:40:52 +0800

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Pavel Naumov on 16.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
