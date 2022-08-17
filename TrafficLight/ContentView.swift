12697808f991f56e1114935c1507c0750cb27d37
malinatrash <malinatrashh@gmail.com>
2022-08-17 16:06:38 +0800

    Release

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -25,28 +25,29 @@ struct ContentView: View {
                 Spacer()
                 
                 Button {
-                    if allLightsDisable {
-                        if redLightOpacity == 1 {
-                            redLightOpacity = 0.5
-                            yellowLightOpacity = 1
-                        } else if yellowLightOpacity == 1 {
-                            yellowLightOpacity = 0.5
-                            greenLightOpacity = 1
-                        } else {
-                            greenLightOpacity = 0.5
-                            redLightOpacity = 1
-                        }
-                    } else {
-                        allLightsDisable.toggle()
-                        redLightOpacity = 1
-                    }
+                    changeLightColor()
                 } label: {
                    TrafficLightButtonView()
                 }
-                
                 Spacer()
-
             }
+        }
+    }
+    private func changeLightColor() {
+        if allLightsDisable {
+            if redLightOpacity == 1 {
+                redLightOpacity = 0.7
+                yellowLightOpacity = 1
+            } else if yellowLightOpacity == 1 {
+                yellowLightOpacity = 0.7
+                greenLightOpacity = 1
+            } else {
+                greenLightOpacity = 0.7
+                redLightOpacity = 1
+            }
+        } else {
+            allLightsDisable.toggle()
+            redLightOpacity = 1
         }
     }
 }
